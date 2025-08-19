815abd493c584f74ff52e0ba1fe721175600cd89
ErenPamukIUE <erenpamuk7@gmail.com>
2025-08-19 22:22:46 +0300

    27 - CSS Custom Style Classes and Selectors

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,7 +13,6 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-import java.awt.*;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
@@ -25,14 +24,15 @@ public class Main extends Application {
     ListView listView;
     TreeView<String> tree1;
     TreeView<String> tree2;
-    TableView<Product> table;
-    TextField nameInput , priceInput , quantityInput;
+    TableView<Product> tableView;
+    TextField nameInput, priceInput, quantityInput;
 
 
     public static void main(String[] args) {
         launch(args);
 
     }
+
     @Override
     public void start(Stage stage) throws Exception {
         window = stage;
@@ -46,15 +46,15 @@ public class Main extends Application {
 
         //                           TABLE VIEW
 
-        TableColumn<Product,String> nameColumn = new TableColumn<>("Name");
+        TableColumn<Product, String> nameColumn = new TableColumn<>("Name");
         nameColumn.setMinWidth(200);
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("Name"));
 
-        TableColumn<Product,Double> priceColumn = new TableColumn<>("Price");
+        TableColumn<Product, Double> priceColumn = new TableColumn<>("Price");
         priceColumn.setMinWidth(200);
         priceColumn.setCellValueFactory(new PropertyValueFactory<>("Price"));
 
-        TableColumn<Product,Integer> quantityColumn = new TableColumn<>("Quantity");
+        TableColumn<Product, Integer> quantityColumn = new TableColumn<>("Quantity");
         quantityColumn.setMinWidth(200);
         quantityColumn.setCellValueFactory(new PropertyValueFactory<>("Quantity"));
 
@@ -81,27 +81,26 @@ public class Main extends Application {
         deleteButton.setOnAction(e -> {
             try {
                 tableDeleteButtonClicked();
-        } catch (Exception exception) {
+            } catch (Exception exception) {
                 AlertBox ab = new AlertBox();
                 ab.setCloseWindowText("Ok");
-                ab.display("Warning",exception.getMessage());
+                ab.display("Warning", exception.getMessage());
 
 
-        } }
-    );
+            }
+        });
 
         HBox tableHBox = new HBox();
-        tableHBox.setPadding(new Insets(10,10,10,10));
+        tableHBox.setPadding(new Insets(10, 10, 10, 10));
         tableHBox.setSpacing(10);
-        tableHBox.getChildren().addAll(nameInput,priceInput,quantityInput,addButton,deleteButton);
+        tableHBox.getChildren().addAll(nameInput, priceInput, quantityInput, addButton, deleteButton);
 
 
-
-        table = new TableView<>();
-        table.setItems(getProduct());
-        table.getColumns().addAll(nameColumn,priceColumn,quantityColumn);
+        tableView = new TableView<>();
+        tableView.setItems(getProduct());
+        tableView.getColumns().addAll(nameColumn, priceColumn, quantityColumn);
         VBox tableVBox = new VBox();
-        tableVBox.getChildren().addAll(table,tableHBox);
+        tableVBox.getChildren().addAll(tableView, tableHBox);
 
         //                           TABLE VIEW
 
@@ -109,24 +108,17 @@ public class Main extends Application {
         //                          COMBO BOX
 
         comboBox = new ComboBox<>();
-        comboBox.getItems().addAll(
-                "Good Will Hunting",
-                "St. Vincent",
-                "Blackhat"
-        );
+        comboBox.getItems().addAll("Good Will Hunting", "St. Vincent", "Blackhat");
         comboBox.setOnAction(e -> System.out.println("User selected: " + comboBox.getValue()));
         comboBox.setEditable(true);
         comboBox.setPromptText("Pick Movie");
         //                          COMBO BOX
 
 
-
         //                           LIST VIEW
         listView = new ListView<>();
-        listView.getItems().addAll("Iron Man","Titanic","Contact","Surrogates");
+        listView.getItems().addAll("Iron Man", "Titanic", "Contact", "Surrogates");
         listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-
-
 
 
         Button viewListButton = new Button("View Selected");
@@ -137,8 +129,6 @@ public class Main extends Application {
         //                           LIST VIEW
 
 
-
-
         //              TREE ROOT AND BRANCHES
 
 
@@ -146,9 +136,9 @@ public class Main extends Application {
         tree1 = new TreeView<>(getTree());
         tree1.setShowRoot(false);
         tree1.setOnMouseClicked(e -> {
-            if(e.getClickCount() == 2 ){
-            TreeItem<String> selectedItem = tree1.getSelectionModel().getSelectedItem();
-                if(selectedItem != null) {
+            if (e.getClickCount() == 2) {
+                TreeItem<String> selectedItem = tree1.getSelectionModel().getSelectedItem();
+                if (selectedItem != null) {
                     System.out.println(selectedItem.getValue());
                 }
             }
@@ -157,8 +147,8 @@ public class Main extends Application {
         //Create Tree2
         tree2 = new TreeView<>(getTree());
         tree2.setShowRoot(false);
-        tree2.getSelectionModel().selectedItemProperty().addListener((v,oldvalue,newValue) -> {
-            if(newValue != null) {
+        tree2.getSelectionModel().selectedItemProperty().addListener((v, oldvalue, newValue) -> {
+            if (newValue != null) {
                 System.out.println(newValue.getValue());
             }
         });
@@ -169,26 +159,24 @@ public class Main extends Application {
         //                         LABEL STYLES
         VBox vBox2 = new VBox();
         Label label1 = new Label("Writing on the wall");
-        //label1.setStyle("-fx-text-fill: Red");
+        label1.setId("bold-label");
         Label label2 = new Label("F10-N4");
         Label label3 = new Label("M2");
-        vBox2.getChildren().addAll(label1,label2,label3);
+        Button dataDiver = new Button("Data Diver");
+        dataDiver.getStyleClass().add("button-fazWrench");
+        vBox2.getChildren().addAll(label1, label2, label3,dataDiver);
 
         //                         LABEL STYLES
 
 
-
-
-
         //                   LAYOUT DESIGN
+        GridPane gridPane = new GridPane();
         VBox vBox = new VBox(10);
-        vBox.setPadding(new Insets(20,20,20,20));
-        vBox.getChildren().addAll(tableVBox,vBox2);
+        vBox.setPadding(new Insets(20, 20, 20, 20));
+        vBox.getChildren().addAll(tableVBox, vBox2);
 
-        Scene scene = new Scene(vBox,1024,700);
-        scene.getStylesheets().add(
-                Main.class.getResource("/sample/EViper.css").toExternalForm()
-        );
+        Scene scene = new Scene(vBox, 1024, 700);
+        scene.getStylesheets().add(Main.class.getResource("/sample/EViper.css").toExternalForm());
         window.setScene(scene);
         window.show();
         //                   LAYOUT DESIGN
@@ -198,18 +186,18 @@ public class Main extends Application {
     //                                  MAIN METHOD END
 
     public TreeItem<String> getTree() {
-        TreeItem<String> root,bucky,megan;
+        TreeItem<String> root, bucky, megan;
         root = new TreeItem<>();
         root.setExpanded(true);
 
-        bucky = makeBranch("Bucky",root);
-        makeBranch("thenewboston",bucky);
-        makeBranch("Youtube",bucky);
-        makeBranch("Chicken",bucky);
+        bucky = makeBranch("Bucky", root);
+        makeBranch("thenewboston", bucky);
+        makeBranch("Youtube", bucky);
+        makeBranch("Chicken", bucky);
 
-        megan = makeBranch("Megan",root);
-        makeBranch("Glitter",megan);
-        makeBranch("Makeup",megan);
+        megan = makeBranch("Megan", root);
+        makeBranch("Glitter", megan);
+        makeBranch("Makeup", megan);
 
         return root;
 
@@ -218,12 +206,12 @@ public class Main extends Application {
 
     //Table Delete Button
     public void tableDeleteButtonClicked() throws Exception {
-        ObservableList<Product> productSelected , allProducts;
-        allProducts = table.getItems();
-        if(allProducts.size() <= 1) {
+        ObservableList<Product> productSelected, allProducts;
+        allProducts = tableView.getItems();
+        if (allProducts.size() <= 1) {
             throw new Exception("You cannot remove the only item on table");
         }
-        productSelected = table.getSelectionModel().getSelectedItems();
+        productSelected = tableView.getSelectionModel().getSelectedItems();
 
         productSelected.forEach(allProducts::remove);
     }
@@ -236,10 +224,10 @@ public class Main extends Application {
         int quantity = product.getQuantity();
 
         name = this.nameInput.getText();
-        if(name.trim().equals("")) {
+        if (name.trim().equals("")) {
             AlertBox ab = new AlertBox();
             ab.setCloseWindowText("Ok");
-            ab.display("Warning" , "Cannot add unnamed item(s).");
+            ab.display("Warning", "Cannot add unnamed item(s).");
             return;
         }
         product.setName(name);
@@ -249,12 +237,13 @@ public class Main extends Application {
         try {
             if (priceTag.equals("")) {
                 product.setPrice(0.0);
-            }else {
-            price = Double.parseDouble(priceTag);}
+            } else {
+                price = Double.parseDouble(priceTag);
+            }
         } catch (NumberFormatException e) {
             AlertBox ab = new AlertBox();
             ab.setCloseWindowText("Ok");
-            ab.display("Warning: Invalid input","'" + priceTag + "' is not a valid price value.");
+            ab.display("Warning: Invalid input", "'" + priceTag + "' is not a valid price value.");
             return;
         }
         product.setPrice(price);
@@ -263,41 +252,42 @@ public class Main extends Application {
         try {
             if (quantityTag.equals("")) {
                 product.setQuantity(0);
-            }else {
-                quantity = Integer.parseInt(quantityTag);}
+            } else {
+                quantity = Integer.parseInt(quantityTag);
+            }
         } catch (NumberFormatException e) {
             AlertBox ab = new AlertBox();
             ab.setCloseWindowText("Ok");
-            ab.display("Warning: Invalid input","'" + quantityTag + "' is not a valid quantity value.");
+            ab.display("Warning: Invalid input", "'" + quantityTag + "' is not a valid quantity value.");
             return;
         }
         product.setQuantity(quantity);
 
-        table.getItems().addAll(product);
+        tableView.getItems().addAll(product);
 
 
     }
 
 
-    public ObservableList<Product> getProduct(){
+    public ObservableList<Product> getProduct() {
         ObservableList<Product> products = FXCollections.observableArrayList();
-        products.add(new Product("Laptop",859.00,20));
-        products.add(new Product("Bear Carpet",3899.00,1));
-        products.add(new Product("Squirrell Statue",1050.00,3));
-        products.add(new Product("Bouncy Ball",55.00,50));
+        products.add(new Product("Laptop", 859.00, 20));
+        products.add(new Product("Bear Carpet", 3899.00, 1));
+        products.add(new Product("Squirrell Statue", 1050.00, 3));
+        products.add(new Product("Bouncy Ball", 55.00, 50));
         return products;
 
 
     }
 
-    public TreeItem<String> makeBranch(String title,TreeItem<String> parent){
+    public TreeItem<String> makeBranch(String title, TreeItem<String> parent) {
         TreeItem<String> item = new TreeItem<>(title);
         item.setExpanded(true);
         parent.getChildren().add(item);
         return item;
     }
 
-    private void viewListButtonClicked(){
+    private void viewListButtonClicked() {
         //for List View
         System.out.println(listView.getSelectionModel().getSelectedItems().stream().collect(Collectors.joining(",")));
     }
@@ -307,7 +297,7 @@ public class Main extends Application {
         System.out.println(comboBox.getValue());
     }
 
-    private void getChoice(ChoiceBox<String> choiceBox){
+    private void getChoice(ChoiceBox<String> choiceBox) {
         //For Choice Box
         String text = choiceBox.getValue();
         System.out.println(text);
@@ -321,10 +311,10 @@ public class Main extends Application {
 
     private boolean isInt(TextField input) {
         String text = input.getText();
-        try{
-        int number = Integer.parseInt(text);
-        System.out.println("Number: " + number +" ;Valid!");
-        return true;
+        try {
+            int number = Integer.parseInt(text);
+            System.out.println("Number: " + number + " ;Valid!");
+            return true;
         } catch (NumberFormatException e) {
             System.err.println("Error: '" + text + "' is not a number");
         }
@@ -333,8 +323,8 @@ public class Main extends Application {
 
     private void closeProgram() {
         ConfirmBox cb = new ConfirmBox();
-        boolean answer = cb.display("Close Program","Are you sure?\nClose Program");
-        if(answer) {
+        boolean answer = cb.display("Close Program", "Are you sure?\nClose Program");
+        if (answer) {
             System.out.println("Closing...");
             window.close();
         }
